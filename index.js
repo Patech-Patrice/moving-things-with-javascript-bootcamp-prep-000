544ed505d586f3ae7be29f2733f4a3d5aa161565
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-09-27 19:33:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function moveDodgerRight() {
     dodger.style.right = `${right - 1}px`;
   }
 }
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
+
+
   
